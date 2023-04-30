33bd23ed665be8316514da789e38be16f5b25f95
gularss92 <gunnar.larsson92@gmail.com>
2023-04-30 23:32:54 +0200

    test
    test

---
+++ src/test/java/com/eksamen/craigslist/CraigslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eksamen.craigslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraigslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
